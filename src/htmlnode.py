49cdac94820a1cab90f16b3fa23c77e4b62b3f22
gdberrio <guilherme.diazberrio@gmail.com>
2025-04-10 22:00:10 +0100

    LeafNode and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -13,8 +13,24 @@ class HTMLNode:
             return ""
         html_doc = ""
         for prop in self.props:
-            html_doc += f" {prop}={self.props[prop]}"
+            html_doc += f' {prop}="{self.props[prop]}"'
         return html_doc
 
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None) -> None:
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("all leafNodes must have a value")
+        if self.tag is None:
+            return self.value
+        html_doc = ""
+        html_doc += f"<{self.tag}{self.props_to_html()}>"
+        html_doc += self.value
+        html_doc += f"</{self.tag}>"
+        return html_doc
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -35,7 +35,7 @@ class TestHTMLNode(unittest.TestCase):
             },
         )
         self.assertEqual(
-            " href=https://www.google.com target=_blank", node.props_to_html()
+            ' href="https://www.google.com" target="_blank"', node.props_to_html()
         )
 
     def test_prop_to_html_none(self):
@@ -55,3 +55,24 @@ class TestHTMLNode(unittest.TestCase):
         )
         with self.assertRaises(NotImplementedError):
             node.to_html()
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_value_none(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_leaf_to_html_tag_none(self):
+        node = LeafNode(None, "text value")
+        self.assertEqual("text value", node.to_html())
+
+    def test_leaf_a_with_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            '<a href="https://www.google.com">Click me!</a>', node.to_html()
+        )
