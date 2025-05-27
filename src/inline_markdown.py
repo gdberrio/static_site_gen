9d39ed859b8c991f84d8a36fb260f132b2bcd45d
gdberrio <guilherme.diazberrio@gmail.com>
2025-05-27 06:42:59 +0100

    starting text to textnodes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -36,3 +36,95 @@ def extract_markdown_links(text):
     regex = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
     matches = re.findall(regex, text)
     return matches
+
+
+def split_nodes_image(old_nodes):
+    split_nodes = []
+    for node in old_nodes:
+        new_nodes = []
+        node_text = node.text
+        while len(node_text) > 0:
+            links = extract_markdown_images(node_text)
+            min = float("inf")
+            first_link_name = None
+            first_link_url = None
+            for link_name, link_url in links:
+                pos = node_text.find(f"![{link_name}]({link_url})")
+                if pos < min:
+                    min = pos
+                    first_link_name = link_name
+                    first_link_url = link_url
+            if first_link_name is None:
+                new_node = TextNode(text=node_text, text_type=TextType.TEXT)
+                new_nodes.append(new_node)
+                break
+            else:
+                splits = node_text.split(
+                    f"![{first_link_name}]({first_link_url})", maxsplit=1
+                )
+                if len(splits) > 1:
+                    before_node = TextNode(text=splits[0], text_type=TextType.TEXT)
+                    new_nodes.append(before_node)
+                    new_nodes.append(
+                        TextNode(
+                            text=first_link_name,
+                            text_type=TextType.IMAGE,
+                            url=first_link_url,
+                        )
+                    )
+                    node_text = splits[1]
+                else:
+                    if node_text:
+                        new_node = TextNode(text=node_text, text_type=TextType.TEXT)
+                        new_nodes.append(new_node)
+                    break
+
+        split_nodes.extend(new_nodes)
+
+    return split_nodes
+
+
+def split_nodes_link(old_nodes):
+    split_nodes = []
+    for node in old_nodes:
+        new_nodes = []
+        node_text = node.text
+        while len(node_text) > 0:
+            links = extract_markdown_links(node_text)
+            min = float("inf")
+            first_link_name = None
+            first_link_url = None
+            for link_name, link_url in links:
+                pos = node_text.find(f"[{link_name}]({link_url})")
+                if pos < min:
+                    min = pos
+                    first_link_name = link_name
+                    first_link_url = link_url
+            if first_link_name is None:
+                new_node = TextNode(text=node_text, text_type=TextType.TEXT)
+                new_nodes.append(new_node)
+                break
+            else:
+                splits = node_text.split(
+                    f"[{first_link_name}]({first_link_url})", maxsplit=1
+                )
+                if len(splits) > 1:
+                    before_node = TextNode(text=splits[0], text_type=TextType.TEXT)
+                    new_nodes.append(before_node)
+                    new_nodes.append(
+                        TextNode(
+                            text=first_link_name,
+                            text_type=TextType.LINK,
+                            url=first_link_url,
+                        )
+                    )
+                    node_text = splits[1]
+                else:
+                    if node_text:
+                        new_node = TextNode(text=node_text, text_type=TextType.TEXT)
+                        new_nodes.append(new_node)
+                    break
+
+        split_nodes.extend(new_nodes)
+
+    return split_nodes
